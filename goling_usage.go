a08984f402310f42ab2383cb4f0b33fadd5d98f5
Gyu-Ho Lee <gyuho.cs@gmail.com>
2013-09-28 02:53:23 -0700

    update

--- goling_usage.go
+++ goling_usage.go
@@ -1,13 +1,14 @@
 /*
-$ export GOPATH=/Users/gyuho/src
-$ cd $GOPATH/Users/gyuho/src/github.com/
-$ go get github.com/golingorg/goling
+http://stackoverflow.com/questions/10130341/go-go-get-go-install-local-packages-and-version-control
 
+$ export GOPATH=/Users/gyuho/src
+$ cd $GOPATH/Users/gyuho/src/
+$ go get github.com/golingorg/goling
 */
 package main
 
 import "fmt"
-import "github.com/golingorg/goling"
+import "goling.go"
 
 func main() {
 	fmt.Pritln(goling.Clean_up("Hdslfkjdsf sdf dsf 	d 		")) 
